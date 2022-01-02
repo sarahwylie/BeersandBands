a34ed179105e761bea495349042727091f5b7362
sarahwylie <sarahannemariawylie@gmail.com>
2022-01-01 18:17:04 -0600

    outputs complete

--- script.js
+++ script.js
@@ -2,41 +2,40 @@ var api = `1dSMNu004yPP163tvnnPbdA3cwZleRuJ`;
 var cityResult = document.querySelector(".city-result")
 var cityInputEl = document.querySelector("#location")
 var cityBtn = document.querySelector(".add-brewery");
-var eventOutput = document.querySelector(".event-results")
+var eventOutput = document.querySelector(".events")
 var startDateInput = document.querySelector("#start-date")
 var endDateInput = document.querySelector("#end-date")
-var breweryOutput = document.querySelector("#breweries-view")
-var starter = new Date(startDateInput)
-document.write(starter.toString())
+var breweryOutput = document.querySelector(".brews")
+// var starter = new Date(startDateInput)
+// document.write(starter.toString())
 
 
 // displaybreweryInfo function re-renders the HTML to display the appropriate content
 function displaybreweryInfo(cityName) {
+  cityInputEl.textContent = "";
     var city = cityName;
   var queryURL =
     `https://api.openbrewerydb.org/breweries?by_city=${city}`;
-  console.log(queryURL);
-  // Creating an AJAX call for the specific brewery button being clicked
+  // console.log(queryURL);
   fetch(queryURL)
   .then(function (response) {
     return response.json();
     })
     .then(function (data) {
-        // Creating a div to hold the brewery
         console.log(data)
 
     // Storing the rating data
     for (i=0; i < data.length; i++){
-      console.log(data[i].name)
+      // console.log(data[i].name)
     // let phone = data[i].phone;
     var name = data[i].name;    
     var type = data[i].brewery_type;
     var street = data[i].street;
     var website = data[i].website_url;
 
-    var breweryCard = document.createElement("span");
+    var breweryCard = document.createElement("div");
     breweryOutput.appendChild(breweryCard);
-
+      
     // var card = document.createElement("div");
     // breweryCard.append(card);
     var naming = document.createElement("h2");
@@ -59,10 +58,7 @@ function displaybreweryInfo(cityName) {
     breweryCard.appendChild(cardGap);
     }
 
-    
-    
-
-
+  
     // <a href="#">@bulmaio</a>. <a href="#">#css</a> <a href="#">#responsive</a>
     // <br>
     // <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
@@ -102,81 +98,82 @@ function displaybreweryInfo(cityName) {
 
     // // Putting the entire brewery above the previous breweries
     // $("#brewerys-view").prepend(breweryDiv);
-    })
+    }) 
     .catch(function (err) {
 
     });
   }
 
-// Function for displaying brewery data
-// function renderButtons() {
-//   // Deleting the breweriess prior to adding new breweries
-//   // (this is necessary otherwise you will have repeat buttons)
-//   $("#buttons-view").empty();
-
-//   // Looping through the array of brewerys
-//   for (var i = 0; i < city.length; i++) {
-//     // Then dynamicaly generating buttons for each brewery in the array
-//     // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
-//     var a = $("<button>");
-//     // Adding a class of brewery-btn to our button
-//     a.addClass("brewery-btn");
-//     // Adding a data-attribute
-//     a.attr("data-name", brewerys[i]);
-//     // Providing the initial button text
-//     a.text(brewerys[i]);
-//     // Adding the button to the buttons-view div
-//     $("#buttons-view").append(a);
-//   }
-// }
-
-// // This function handles events where a brewery button is clicked
-// $("#add-brewery").on("click", function (event) {
-//   event.preventDefault();
-//   // This line grabs the input from the textbox
-//   var brewery = $("#brewery-input").val().trim();
-
-//   // Adding brewery from the textbox to our array
-//   brewerys.push(brewery);
-
-//   // Calling renderButtons which handles the processing of our brewery array
-//   renderButtons();
-// });
-
-// Adding a click event listener to all elements with a class of "brewery-btn"
-// $(document).on("click", ".brewery-btn", displaybreweryInfo);
-
-// Calling the renderButtons function to display the initial buttons
-// renderButtons();
-
-// $("#thing_to_click").on("click", function () {
-//   window.location = "./results.html";
-// });
 var getEventResults = (city, startDate, endDate) => {
-  //api url
-  var TapiUrl = `https://app.ticketmaster.com/discovery/v2/events.json?startDateTime=${startDate}&endDateTime=${endDate}&city=${city}&apikey=${api}`;
-console.log(TapiUrl);
-  //make a request to the url
-  fetch(TapiUrl)
-      .then(function (response) {
-          return response.json();
-      }).then(function (res) {
-          console.log(res);
+  cityInputEl.textContent = "";
+  $.ajax({
+    type:"GET",
+    url:`https://app.ticketmaster.com/discovery/v2/events.json?apikey=${api}&city=${city}`,
+    async:true,
+    dataType: "json",
+    success: function(json) {
+      let data = json._embedded.events
+                console.log(data);
+
+    for (i=0; i < data.length; i++){
+      // console.log(data[i].name)
+
+    var name = data[i].name;    
+    var pic = data[i].images[0].url;
+    var venue = data[i]._embedded.venues.name;
+      console.log(venue)
+    var url = data[i].url;
+
+    var eventCard = document.createElement("div");
+    eventOutput.appendChild(eventCard);           
+  
+    var naming = document.createElement("h2");
+    eventCard.appendChild(naming);
+    naming.textContent = name;
+  
+    var eventVenue = document.createElement("p");
+    eventVenue.textContent = venue;
+    eventCard.appendChild(eventVenue);
+
+    var photo = document.createElement("img");
+    photo.src = `${pic}`
+    // console.log(photo)
+    eventCard.appendChild(photo);
+
+    var web = document.createElement("p")
+    web.textContent = url;
+    eventCard.appendChild(web);
+
+    var cardGap = document.createElement("br");
+    eventCard.appendChild(cardGap);
+  }
+  //   //api url
+//   var TapiUrl = `https://app.ticketmaster.com/discovery/v2/events.json?startDateTime=${startDate}&endDateTime=${endDate}&city=${city}&apikey=${api}`;
+// console.log(TapiUrl);
+//   //make a request to the url
+//   fetch(TapiUrl)
+//       .then(function (response) {
+//           return response.json();
+//       }).then(function (res) {
+//           console.log(res);
+//     .catch(function (err) {
+            }   
 });
-};
+  }
 
 var formSubmitHandler = (event) => {
     event.preventDefault();
     var city = cityInputEl.value.trim();
-    var startDate = starter.value;
+    var startDate = startDateInput.value;
     // startDate = Date.parse("YYYY-MM-DDTHH:mm:ssZ");
-    var start = new Date(startDate);
+    // var start = new Date(startDate);
      var endDate = endDateInput.value;
-    var end = new Date(endDate)
+    // var end = new Date(endDate)
     // end.toString()
-      console.log(start, end) 
+      console.log(startDate, endDate) 
       displaybreweryInfo(city);
        getEventResults(city, startDate, endDate);
-};
+  
+  };
 
 cityBtn.addEventListener("click", formSubmitHandler);
